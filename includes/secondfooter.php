2dd8309c5c9ef45ecd85c967b0721a1517a4e14f
Tutu-an <83591086+Tutu-an@users.noreply.github.com>
2022-03-28 23:16:10 +0200

    Add files via upload

---
+++ includes/secondfooter.php
@@ -0,0 +1,37 @@
+<footer class="page-footer">
+    <div class="container footer-info">
+        <div class="row">
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Github:</h5>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://github.com/Tutu-an/ecommerceVetements"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>Tuan LA</a></li>
+                    <!-- <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://smakosh.com/"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>Ismail Ghallou</a></li> -->
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Categories</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">Chemise</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">Pantalon</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">Tee-shirt</a></li>
+                </ul>
+            </div>
+
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Nos Magasins:</h5></br>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="">22 Av. d'Italie, 75013 Paris</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="">17 Rue Scribe, 75009 Paris</a></li>
+                    <!-- <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartSpeaker</a></li> -->
+                    <!-- <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartVR</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © <?= Date('Y'); ?> All rights reserved
+            <!-- <a class="grey-text text-lighten-4 right animated slideInUp wow" href="https://smakosh.com/"><i class="fa fa-heartbeat animated pulse infinite red-text"></i>Tuan LA</a> -->
+        </div>
+    </div>
+</footer>
